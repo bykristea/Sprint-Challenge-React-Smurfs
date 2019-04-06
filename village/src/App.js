0bb40d2966ff9335bd9cfc92b05e7a666add748a
Kristea Gamel <krgamel@gmail.com>
2019-04-06 12:03:44 -0500

    Route, Router and NavLink
    wrapped root component in router, declared route and created two routes in app.js.

--- village/src/App.js
+++ village/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Nav from './components/Nav';
 
 class App extends Component {
   constructor(props) {
@@ -32,8 +34,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm fetchSmurfs={this.fetchSmurfs} />
-        <Smurfs smurfs={this.state.smurfs} />
+        {/* <SmurfForm fetchSmurfs={this.fetchSmurfs} />
+        <Smurfs smurfs={this.state.smurfs} /> */}
+
+        <Route path="/" component={Nav} />
+        <Route exact path="/" render={props => 
+        <Smurfs {...props} smurfs={this.state.smurfs} />} 
+        />
+        <Route path="/smurf-form" render={props =>
+        <SmurfForm {...props} fetchSmurfs={this.fetchSmurfs} />}
+        />
       </div>
     );
   }
